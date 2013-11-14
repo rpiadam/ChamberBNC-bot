86e8461606871791a3fa63ce4c6473cd6400679a
Andrew Northall <andy@somasonic.org>
2013-11-14 18:40:59 +0000

    more progress

--- bncim-bot.rb
+++ bncim-bot.rb
@@ -11,7 +11,41 @@
 $:.unshift File.dirname(__FILE__)
 
 require 'cinch'
+require 'yaml'
 require 'lib/requests'
 
-# unfinished
+$config = YAML.load_file("config/config.yaml")
+$bots = Hash.new
+$threads = Array.new
 
+# Set up a bot for each server
+$config["servers"].each do |name, server|
+  bot = Cinch::Bot.new do
+    configure do |c|
+      c.nick = "bncim-test"
+      c.user = "bncim"
+      c.realname = "bnc.im administration bot"
+      c.server = server["server"]
+      c.ssl.use = server["ssl"]
+      c.sasl.username = "bncim"
+      c.sasl.password = $config["nickserv-password"]
+      c.port = server["port"]
+      c.channels = ["#bnc.im-admin"]
+      c.plugins.plugins = [RequestPlugin]
+    end
+  end
+  bot.loggers << Cinch::Logger::FormattedLogger.new(File.open("log/#{name}.log", "a"))
+  bot.loggers.level = :info
+  $bots[name] = bot
+end
+
+# Initialize the RequestDB
+RequestDB.load($config["requestdb"])
+
+# Start the bots
+$bots.each do |key, bot|
+  puts "Starting #{key} bot..."
+  $threads << Thread.new { bot.start }
+end
+
+$threads.each { |t| t.join } # wait for other threads
--- lib/requests.rb
+++ lib/requests.rb
@@ -16,11 +16,11 @@ class RequestDB
 
 	def self.load(file)
 		CSV.foreach(file) do |row|
-			request = Request.new(row[0].to_i, row[3], row[4], row[5], \
-													 row[6], row[1].to_i)
+			request = Request.new(row[0].to_i, row[3], row[7], row[4], \
+													 row[5], row[6], row[1].to_i)
 			request.key = row[2]
-			request.approved = row[7]
-			request.confirmed = row[8]
+			request.approved = row[8]
+			request.confirmed = row[9]
 			@@requests[request.id] = request
 		end
 		@@requests.each { |id, req| puts "Request #{id} loaded: " +  
@@ -32,7 +32,7 @@ class RequestDB
 		csv_string = CSV.generate do |csv|
 			@@requests.each_value do |r|
 				csv << [r.id, r.ts, r.key, r.source, r.email, r.server, \
-					r.port, r.approved?, r.confirmed?]
+					r.port, r.username, r.approved?, r.confirmed?]
 			end
 		end
 		file.write csv_string
@@ -46,9 +46,12 @@ class RequestDB
 	def self.create(*args)
 		obj = Request.new(self.next_id, *args)
 		@@requests[obj.id] = obj
+		RequestDB.save($config["requestdb"])
+    @@requests[obj.id]
 	end
 
 	def self.next_id
+    return 1 if @@requests.empty?
 		max_id_request = @@requests.max_by { |k, v| k }
 		max_id_request[0] + 1
 	end
@@ -59,25 +62,33 @@ class RequestDB
 
 	def self.confirm(id, confirmed = true)
 		@@requests[id].confirmed = confirmed
+		RequestDB.save($config["requestdb"])
 	end
 
 	def self.approve(id, approved = true)
 		@@requests[id].approved = approved
+		RequestDB.save($config["requestdb"])
+	end
+
+	def self.delete_id(id)
+		@@requests.delete id
+		RequestDB.save($config["requestdb"])
 	end
 end
 
 class Request
-	attr_reader :id
+	attr_reader :id, :username
 	attr_accessor :key, :ts, :approved, :confirmed
 	attr_accessor :source, :email, :server, :port
 
-	def initialize(id, source, email, server, port, ts = nil)
+	def initialize(id, source, username, email, server, port, ts = nil)
 		@id = id
 		@ts = ts || Time.now.to_i
 		@key = RequestDB.gen_key(15)
 		@approved = false
 		@confirmed = false
 		@source = source
+		@username = username
 		@email = email
 		@server = server
 		@port = port
@@ -92,3 +103,52 @@ class Request
 	end
 end
 
+class RequestPlugin
+	include Cinch::Plugin
+	match /request\s+(\w+)\s+(\S+)\s+(\S+)\s+(\+?\d+)/, method: :request, group: :request
+	match /request/, method: :help, group: :request
+  
+  match /delete\s+(\d+)/, method: :delete, group: :admin
+  match /reqinfo\s+(\d+)/, method: :reqinfo, group: :admin
+
+	def request(m, username, email, server, port)
+		r = RequestDB.create(m.user.mask, username, email, \
+															 server, port)
+                               
+    reply = "%s %s Source: %s / Date: %s / Sever: %s / Port: %s / Approved: %s" % 
+     [Format(:red, "[NEW]"), Format(:bold, "[##{r.id}]"), 
+      Format(:bold, r.source.to_s), Format(:bold, Time.at(r.ts).ctime), 
+      Format(:bold, r.server), Format(:bold, r.port.to_s), 
+      Format(:bold, r.approved?.to_s)]                         
+    
+    Channel("#bnc.im-admin").send reply
+	end
+  
+  def reqinfo(m, id)
+    return unless m.channel == "#bnc.im-admin"
+    r = RequestDB.requests[id.to_i]
+    
+    if r.nil?
+      m.reply "Request ##{id} not found."
+      return
+    end
+    
+    reply = "%s Source: %s / Date: %s / Sever: %s / Port: %s / Approved: %s" % 
+      [Format(:bold, "[##{r.id}]"), Format(:bold, r.source.to_s), 
+       Format(:bold, Time.at(r.ts).ctime), Format(:bold, r.server), 
+       Format(:bold, r.port.to_s), Format(:bold, r.approved?.to_s)]                         
+    
+    m.reply reply
+  end
+  
+  def delete(m, id)
+    return unless m.channel == "#bnc.im-admin"
+    RequestDB.delete_id id.to_i
+    m.reply "Deleted request ##{id}."
+  end
+  
+  def help(m)
+    m.reply "Invalid syntax. Syntax: !request <user> <email> <server> [+]<port>"
+    m.reply "For example, a user called bncim-lover with an email of ilovebncs@mail.com who wants a bouncer for Interlinked would issue: !request bncim-lover ilovebncs@mail.com irc.interlinked.me 6667"
+  end
+end
