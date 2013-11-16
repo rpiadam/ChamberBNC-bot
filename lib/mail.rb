65644305d4108eae3ee5ccccb04300bc506d6a4a
Andrew Northall <andy@somasonic.org>
2013-11-16 09:46:24 +0000

    Add account approved mail

--- lib/mail.rb
+++ lib/mail.rb
@@ -24,23 +24,69 @@ class Mail
 
   def self.send_verify(to_addr, id, code)
     
-    msgstr = <<END_OF_MESSAGE
-From: bnc.im bot <no-reply@bnc.im>
-To: #{to_addr}
-Subject: bnc.im account verification
-Date: #{Time.now.ctime}
-Message-Id: <#{UUID.generate}@bnc.im>
+    msgstr = <<-END_OF_MESSAGE
+      From: bnc.im bot <no-reply@bnc.im>
+      To: #{to_addr}
+      Reply-to: admin@bnc.im
+      Subject: bnc.im account verification
+      Date: #{Time.now.ctime}
+      Message-Id: <#{UUID.generate}@bnc.im>
 
-Someone, hopefully you, requested an account in the http://bnc.im IRC channel. If this was you, please send
+      Someone, hopefully you, requested an account in the http://bnc.im IRC channel. If this was you, please send
 
-  !verify #{id} #{code} 
+      !verify #{id} #{code} 
 
-in either #bnc.im or in a private message to the bot. If you need any help, please visit http://bnc.im/webchat.html.
+      in either #bnc.im or in a private message to the bot. If you need any help, please visit http://bnc.im/webchat.html.
 
-Regards,
-bnc.im team
-END_OF_MESSAGE
+      Regards,
+      bnc.im team
+			http://bnc.im/
+    END_OF_MESSAGE
+    
+    # remove whitespace from above
+    msg = msgstr.lines.map { |l| l.strip }.join("\r\n")
 
-    self.send(to_addr, msgstr)
+    self.send(to_addr, msg)
   end
+  
+  def self.send_approved(to_addr, server, addr, webpanel, user, pass)
+    
+    msgstr = <<-END_OF_MESSAGE
+      From: bnc.im bot <no-reply@bnc.im>
+      To: #{to_addr}
+      Reply-to: admin@bnc.im
+      Subject: bnc.im account approved
+      Date: #{Time.now.ctime}
+      Message-Id: <#{UUID.generate}@bnc.im>
+      
+      Dear #{user},
+      
+      Your bnc.im account has been approved. Your account details are:
+      
+      Server: #{server} (#{addr})
+      Username: #{user}
+      Password: #{pass}
+      Web Panel: #{webpanel}
+      
+      In order to connect to your new account, you will need to connect
+      your IRC client to #{addr} on port 6667 (or 6697 for SSL) and
+      configure your IRC client to send your username and password 
+      together in the server password field, seperated by a colon,
+      like so:  #{user}:#{pass}
+      
+      If you need any help, please do not hestitate to join our IRC 
+      channel: irc.interlinked.me #bnc.im - or connect to our webchat
+      at https://bnc.im/webchat.html.
+      
+      Regards,
+      bnc.im team
+			http://bnc.im/
+    END_OF_MESSAGE
+    
+    # remove whitespace from above
+    msg = msgstr.lines.map { |l| l.strip }.join("\r\n")
+
+    self.send(to_addr, msg)
+  end
+      
 end
